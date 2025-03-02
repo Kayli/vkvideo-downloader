e11d40be447c178d8911593a18e014fdef64251a
illiam <ilichpost@gmail.com>
2025-03-02 13:20:44 -0800

    Clean up main.py by removing unnecessary imports

--- src/app/main.py
+++ src/app/main.py
@@ -1,13 +1,3 @@
-import os
-import sys
-import yaml
-import argparse
-from typing import List, Dict, Optional, Union
-
-# Import classes from other modules
-from .extractor import Extractor
-from .exporter import VideoLinkExporter, OUTPUT_YAML_FILE
-from .logger import Logger
 from .cli_app import CLIApp
 
 if __name__ == '__main__':
